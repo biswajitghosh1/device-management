e9009947303de0a3be5be0c21d3d278189a1bd88
Biswajit Ghosh <92691661+biswajitghosh1@users.noreply.github.com>
2024-12-18 22:24:28 +0530

    Updated the Scripts folder.

---
+++ scripts/macOS - DefermacOSMajorUpgrade_30Days.zsh
@@ -0,0 +1,16 @@
+#This script will defer the macOS upgrade by 30 days and needs to be deployed via Workspace One. Initially this script will be applied to the group of MacBooks if required, simnce this will only temporarily block the updates
+
+#----COPY BELOW THIS LINE------
+
+
+
+#!/bin/zsh
+
+outputPath="/Library/Application Support/macOSUpdate/"
+outputlog=$outputPath"daycount.txt"
+softwareupdatelog=$outputPath"softwareupdate.txt"
+DefaultlatestVersion=$(awk -F , '$2>max {max=$2;output=$2}END{print output}' "$softwareupdatelog" | cut -d ' ' -f3)
+OSname=$(grep "$DefaultlatestVersion," "$softwareupdatelog" | awk -F ',' 'END{print substr($1,16,19)}')
+
+datediff=$(head -1 "$outputlog")
+expireday=$(expr 30 - $datediff)
